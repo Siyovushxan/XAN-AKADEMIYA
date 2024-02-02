3a816f550038bf224b0461379d134e146d4c5dcc
siyovushxan <siyovushxan@gmail.com>
2024-02-02 16:21:00 +0500

    Yuklash

---
+++ js/index.js
@@ -0,0 +1,9 @@
+let bulimOne = document.getElementById('menyu-dars_item-one')
+
+function ochilish() {
+    if (bulimOne.style.display === 'none') {
+        bulimOne.style.display = 'block'
+    } else {
+        bulimOne.style.display = 'none'
+    }
+}
